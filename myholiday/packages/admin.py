c9471c966d9c99c723e42eb6449f28b33c34b479
Vicky185 <vstevenson93@gmail.com>
2020-08-01 20:40:58 +0000

    customised packages on admin

--- myholiday/packages/admin.py
+++ myholiday/packages/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from .models import Package, Category
 
 # Register your models here.
-admin.site.register(Package)
-admin.site.register(Category)
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'destination',
+        'departs',
+        'category',
+        'price',
+        'rating',
+        'image',
+        'image_url',
+        'cover',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Package, PackageAdmin)
+admin.site.register(Category, CategoryAdmin)
--- myholiday/packages/models.py
+++ myholiday/packages/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250, null=True, blank=True)
 
