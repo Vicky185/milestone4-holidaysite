b972b828fb8d327d4b7a3c528da7e7cf4e2a39b4
Vicky185 <vstevenson93@gmail.com>
2020-08-03 11:16:52 +0000

    added shopping cart functionality

--- myholiday/shoppingcart/views.py
+++ myholiday/shoppingcart/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def view_cart(request):
     """ a view that renders the shopping cart contents page """
     return render(request, 'shoppingcart/cart.html')
-    
+
+def add_to_cart(request, item_id):
+    """ add a quantity of the specific package item to the shopping bag"""
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
