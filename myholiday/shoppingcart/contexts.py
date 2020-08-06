0a214848988df93f8c842cceae6d700db3d77254
Vicky185 <vstevenson93@gmail.com>
2020-08-06 14:34:08 +0000

    adjusted cart to add in adjust quantity of cart, remove and update items

--- myholiday/shoppingcart/contexts.py
+++ myholiday/shoppingcart/contexts.py
@@ -1,15 +1,32 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from packages.models import Package
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     package_count = 0
+    cart = request.session.get('cart', {})
 
+    for item_id, quantity in cart.items():
+        package = get_object_or_404(Package, pk=item_id)
+        total += quantity * package.price
+        package_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'package': package,
+        })
+    
+    grand_total = total
+    
     context = {
-       'cart_items': cart_items,
-       'total': total,
-       'package_count': package_count,
+        'cart_items': cart_items,
+        'total': total,
+        'package_count': package_count,
+        'grand_total': grand_total,
     }
+
     return context
--- myholiday/shoppingcart/views.py
+++ myholiday/shoppingcart/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
+
 def view_cart(request):
-    """ a view that renders the shopping cart contents page """
+    """ A view that renders the cart contents page """
+
     return render(request, 'shoppingcart/cart.html')
 
 def add_to_cart(request, item_id):
-    """ add a quantity of the specific package item to the shopping bag"""
+    """ Add a quantity of the specified product to the shopping cart """
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -17,5 +20,36 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified items to specific amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        del cart[item_id]
+        if not cart[item_id]:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+def remove_from_cart(request, item_id):
+    """ Remove item from the cart """
+    try:
+        cart = request.session.get('cart', {})
+        if cart:
+            del cart[item_id]
+        else:
+            cart.pop[item_id]
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        return HttpResponse(status=500)
+
