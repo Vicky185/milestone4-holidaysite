cb189fee6562be5840192aeee4d1413d266d7c6d
Vicky185 <vstevenson93@gmail.com>
2020-08-08 10:55:12 +0000

    added stripe functionality

--- myholiday/checkout/views.py
+++ myholiday/checkout/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from shoppingcart.contexts import cart_contents
+
+import stripe
 
 # Create your views here.
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "there's nothing in your cart at the moment")
         return redirect(reverse('packages'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HAIE2KM1us24aM9vfOHClJG14Xatx3oNqkfvZG4hpJYCGO2KIidZjGvrD81I8iHn4tstj5WppOjkU25rnrxyBVU00hUuVJSKU',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
