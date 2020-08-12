8b8f6d0f48beebcd868de094aa680d429cd9d179
Vicky185 <vstevenson93@gmail.com>
2020-08-12 20:42:39 +0000

    added confirmation emails upon purchases

--- myholiday/checkout/webhook_handler.py
+++ myholiday/checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from packages.models import Package
@@ -13,6 +16,19 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """ Send user confirmation email """
+        cust_email = order.email
+        subject = render_to_string('../templates/confirmation_emails/confirmation_email_subject.txt', {'order': order})
+        body = render_to_string('../templates/confirmation_emails/confirmation_email_body.txt', {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
 
     def handle_event(self, event):
         """ Handle a generic/unknown/unexpected webhook event """
@@ -74,6 +90,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database', status=200)
         else:
@@ -107,7 +124,7 @@ class StripeWH_Handler:
                 if order:
                     order.delete()
                 return HttpResponse(content=f'Webhook received: {event["type"]} | ERROR: {e}', status=500)
-
+        self._send_confirmation_email(order)
         return HttpResponse(content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook', status=200)
 
     def handle_payment_intent_payment_failed(self, event):
