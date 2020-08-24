836cc4e43e659549628dbcd83e12fe7deb5e34fa
Vicky185 <vstevenson93@gmail.com>
2020-08-24 20:55:57 +0000

    prep deploy

--- myholiday/myholiday/wsgi.py
+++ myholiday/myholiday/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myholiday.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myholiday.myholiday.settings')
 
 application = get_wsgi_application()
